aa01d9ba371bc086dd9db19f592e356232eff627
unknown <morkocberk@gmail.com>
2021-05-31 00:22:53 +0300

    slider effect to the main page with agile

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,92 @@
 <template>
-  <main class="container">
-    <header>
-      <h1>Home Page</h1>
-      <!--<h4>And it's made with Nuxt.</h4>-->
-      <!--<img src="~/static/owen.png" alt="Bot ADS" />-->
-    </header>
-    <section>
-      <h2>Content</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum
-      </p>
-    </section>
-  </main>
+  <agile :nav-buttons="false" :dots="false" :autoplay-speed="5000" :speed="2500" fade pause-on-dots-hover autoplay>
+      <div class="slide" v-for="n in 2" :key="n" :class="`slide-${n}`">    
+        <h2 class="slide-element sl-45" slot="caption">{{ captions[n] }}</h2>    
+        <p class="slide-element sl-55" slot="description">{{descriptions[n]}}</p>
+      </div>
+    </agile>
 </template>
+
+
 
 <script>
 export default {
+  data () {
+        return {            
+            captions: [
+              '',
+              'This is slide 1',
+              'This is a third and final slide',
+            ],
+            descriptions:[
+              '',
+              'Description 1 ',
+              'Final description',
+            ]
+        }
+  },
   created() {
     const breadcrumbs = []
     this.$store.commit('SET_BREADCRUMBS', breadcrumbs)
-  },
+  }
 }
 </script>
 
 <style scoped>
+.slide-element{
+  position: fixed;
+  left:10%;
+}
+
+.sl-45{
+  top:45%;
+}
+
+.sl-55{
+  top:55%;
+}
+
+.agile__dots {
+  bottom: 10px;
+  flex-direction: column;
+  right: 30px;
+  position: absolute; }
+
+.agile__dot {
+  margin: 5px 0; }
+  .agile__dot button {
+    background-color: transparent;
+    border: 1px solid #fff;
+    cursor: pointer;
+    display: block;
+    height: 10px;
+    font-size: 0;
+    line-height: 0;
+    margin: 0;
+    padding: 0;
+    transition-duration: .3s;
+    width: 10px; }
+  .agile__dot--current button, .agile__dot:hover button {
+    background-color: #fff; }
+
+.slide-1{
+  background-image: url('https://images.unsplash.com/photo-1509549649946-f1b6276d4f35?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ');
+}
+.slide-2{
+  background-image: url('https://images.unsplash.com/photo-1511469054436-c7dedf24c66b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&fit=max&ixid=eyJhcHBfaWQiOjEyMDd9');
+}
+
+.slide {
+  align-items: center;
+	color:#fff;
+	display: block;
+	/* justify-content: center; */
+  height: 600px;
+  width: 100%;
+  }
+
 h2 {
   margin-bottom: 30px;
-}
-h4 {
-  margin-bottom: 30px;
-}
-img {
-  max-width: 600px;
-}
-p {
-  text-align: left;
+  font-weight: bold;
 }
 </style>
